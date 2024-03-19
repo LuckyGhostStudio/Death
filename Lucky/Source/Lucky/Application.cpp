f2297016fae4e137a0dc63af14c3da49672a56b2
LuckyGhostStudio <1398346167@qq.com>
2024-03-19 21:34:33 +0800

    Added Event System

--- Lucky/Source/Lucky/Application.cpp
+++ Lucky/Source/Lucky/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Lucky
 {
 	Application::Application()
@@ -12,6 +15,16 @@ namespace Lucky
 
 	void Application::Run()
 	{
+		// Test
+		WindowResizeEvent e(1280, 720);
+
+		if (e.IsInCategory(EventCategoryApplication)) {
+			LC_TRACE(e.ToString());
+		}
+		if (e.IsInCategory(EventCategoryInput)) {
+			LC_TRACE(e.ToString());
+		}
+
 		while (true);
 	}
 }
--- Lucky/Source/Lucky/Core.h
+++ Lucky/Source/Lucky/Core.h
@@ -4,3 +4,5 @@
 #ifdef LC_PLATFORM_WINDOWS
 	// TODO:
 #endif
+
+#define BIT(x) (1 << x)		// 1×óÒÆxÎ»
